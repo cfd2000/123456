a9f8b97e5c244ecccd487892e1b1b6f192bd2098
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2020-11-07 07:19:01 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$UserAgent = "Mozilla/5.0 (Linux; Android 5.1.1; Andromax B16C2G Build/LMY47V) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36";
+$UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36";
 $AllCookie = "__cfduid=d2373b191c915a6806d09b27ec0752c841604583411;_ga=GA1.2.631308681.1604583422;_luid=1514235;_gid=GA1.2.355453795.1604583422;_lrtk=z7fex2aMhgtQBwKothc8SCen3ZmCSQjTFwaQSxcs5dKGbAJoQSF1YzYZqaMXnZYn";
 
 ?>
